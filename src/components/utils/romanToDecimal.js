aa41ef0c6b3ef750f81637db8ebced905f17422e
BondoS <eng.serag00@gmail.com>
2020-01-26 19:17:43 +0100

    Fix Combining Overline letter issue

--- src/components/utils/romanToDecimal.js
+++ src/components/utils/romanToDecimal.js
@@ -1,8 +1,23 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable no-return-assign */
 import map from './romanMap';
 
 export default roman => {
-  return roman.split('').reduce((acc, curr, i, arr) => {
+  const numArr = [];
+
+  for (let i = 0; i < roman.length; i++) {
+    // check if the next letter is ◌̅  then add current and the next as just on character
+    // https://www.compart.com/en/unicode/U+0305
+    if (/[\u0305]/.test(roman.slice(i + 1, i + 2))) {
+      numArr.push(roman.slice(i, i + 2));
+      i++;
+    } else {
+      numArr.push(roman.slice(i, i + 1));
+    }
+  }
+  console.log('numArr', numArr);
+  return numArr.reduce((acc, curr, i, arr) => {
+    // console.log('curr', curr);
     // if the current number is less than the next number
     return map[curr] < map[arr[i + 1]]
       ? // subtract it from the accumulative
